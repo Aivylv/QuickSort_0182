1b1df3af194c40dac00a309d622c9b6f64edf6b2
aivylv <lyvialarasati@gmail.com>
2024-03-28 11:19:36 +0700

    kontruksi IF untuk swap elemen pada indeks i dgn elemen pada indeks j

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -63,6 +63,12 @@ void q_short(int low, int high)
 			j--; //step 8
 			cmp_count++;
 		}
+		cmp_count++;
+		if (i < j) //step 9
+		{
+			//swap the element at index i with the element at index j
+			swap(i, j);
+		}
 	}
 }
 int main()
